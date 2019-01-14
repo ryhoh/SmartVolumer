70851773772590be9a06a720add60c3c17088632
ryhoh <axis38akasira@gmail.com>
2019-01-14 09:23:37 +0900

    モデル修正
    二次関数と一次関数のいいとこ取り関数に変更．
    ノートブックとアプリ両側に修正．

--- app/src/main/java/com/gmail/axis38akasira/autovolumer/RegressionModel.java
+++ app/src/main/java/com/gmail/axis38akasira/autovolumer/RegressionModel.java
@@ -1,9 +1,22 @@
 package com.gmail.axis38akasira.autovolumer;
 
 class RegressionModel {
-    private static double[] beta = {0.0176811594, 2.31544266, -7.75456428};
+    private static double[] beta1 = {0.0326086957, 1.46245059};
+    private static double[] beta2 = {0.0176811594, 2.31544266, -7.75456428};
 
     static Double infer(double x) {
-        return beta[0] + beta[1] * x + beta[2] * x * x;
+        if (x <= 0.06) {
+            return f_2(x);
+        } else {
+            return Math.max(f_1(x), f_2(x));
+        }
+    }
+
+    private static Double f_1(double x) {
+        return beta1[0] + beta1[1] * x;
+    }
+
+    private static Double f_2(double x) {
+        return beta2[0] + beta2[1] * x + beta2[2] * x * x;
     }
 }
