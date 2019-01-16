e284462f8b0c0f9552bb99d2068b23301f0bbcea
ryhoh <axis38akasira@gmail.com>
2019-01-16 09:47:15 +0900

    リファクタリング
    MainActivityをコンパクトにするため，Handlerにより定期的に実行される部分（騒音レベル取得と音量調整）の部分を匿名クラスから別クラスに分けた．

--- app/src/main/java/com/gmail/axis38akasira/autovolumer/MainActivity.java
+++ app/src/main/java/com/gmail/axis38akasira/autovolumer/MainActivity.java
@@ -25,12 +25,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 128;
+    final static int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 128;
 
-    AudioManager am;
-    AudioRecord ar;
-    boolean micAccessAllowed = false;
-    boolean autoEnabled = false;
+    static AudioManager am;
+    static AudioRecord ar;
+    static boolean micAccessAllowed = false;
+    static boolean autoEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,84 +83,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private int init_audioRecord() {
-        int buffSize = 0;
-        for (short audioFormat: new short[] {AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT}) {
-            for (short channelConfig: new short[] {AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO}) {
-                try {
-                    buffSize = AudioRecord.getMinBufferSize(8000,
-                            channelConfig, audioFormat);
-                    ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000,
-                            channelConfig, audioFormat, buffSize);
-                    if (ar.getState() == AudioRecord.STATE_INITIALIZED) {
-                        break;
-                    }
-                } catch (Exception e) {}
-            }
-        }
-
-        if (buffSize == AudioRecord.ERROR_BAD_VALUE || buffSize == AudioRecord.ERROR) {
-            throw new IllegalStateException();
-        }
-
-        return buffSize;
-    }
-
     private void handler_audioRecord() {
         final TextView textView_playingVol = findViewById(R.id.tv_playingVol);
         final TextView textView_envVol = findViewById(R.id.tv_envVol);
 
         final Handler handler = new Handler();
-        handler.post(new Runnable() {
-            int micSenseCnt = 0, micSenseSum = 0;
-            int buffSize;
-            short[] buffer;
-            @Override
-            public void run() {
-                if (micAccessAllowed) {
-                    // マイク入力用 AudioRecord の設定
-                    if (ar == null) {
-                        buffSize = init_audioRecord();
-                        buffer = new short[buffSize];
-                        ar.startRecording();
-                    }
-
-                    if (ar.read(buffer, 0, buffSize) < 0) {
-                        // 起動に失敗する場合，デバイスの設定->アプリ->権限->マイクを許可
-                        throw new IllegalStateException();
-                    }
-                    // 最大
-                    int max_val = Integer.MIN_VALUE;
-                    for (short x : buffer) {
-                        max_val = Math.max(max_val, x);
-                    }
-
-                    // 何度も計測して，平均値をその時間間隔の間の計測結果とする
-                    micSenseSum += max_val;
-                    if (micSenseCnt != 9) micSenseCnt++;
-                    else {
-                        final double inputLevel = micSenseSum / 10.0;
-                        micSenseSum = 0;
-                        micSenseCnt = 0;
-
-                        textView_envVol.setText(String.valueOf(inputLevel));
-
-                        if (autoEnabled) {
-                            // 耳を護るために上限を設ける
-                            double outLevel = RegressionModel.infer(inputLevel / 100000);
-                            outLevel = Math.min(outLevel, 0.25);
-                            // 下限も設定して音が消えないようにする
-                            final int i_outLevel = (int) Math.max(Math.round(outLevel * am.getStreamMaxVolume(AudioManager.STREAM_MUSIC)), 1);
-
-                            // 再生音量を変更し，TextViewにも反映
-                            am.setStreamVolume(AudioManager.STREAM_MUSIC, i_outLevel, 0);
-                            textView_playingVol.setText(String.valueOf(i_outLevel));
-                        }
-                    }
-                }
-                handler.postDelayed(this, 100);
-            }
-        });
+        handler.post(new VolumeManager(handler, textView_envVol, textView_playingVol));
     }
 
     private void init_buttonToggleMode() {
