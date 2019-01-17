f7c2cbdd8b344230e21e088ec4279821513c6e27
ryhoh <axis38akasira@gmail.com>
2019-01-17 15:41:09 +0900

    機能追加
    音量の自動調整がONになっている時，通知欄に削除不可能な通知を表示するようにした．

--- app/src/main/java/com/gmail/axis38akasira/autovolumer/MainActivity.java
+++ app/src/main/java/com/gmail/axis38akasira/autovolumer/MainActivity.java
@@ -1,10 +1,15 @@
 package com.gmail.axis38akasira.autovolumer;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +17,7 @@ import android.os.Handler;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -21,9 +27,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     final static int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 128;
+    final String NOTIFICATION_CHANNEL_ID = "appearEnable";
+    final int NOTIFICATION_REQUEST_CODE = 1024;
+    final int NOTIFICATION_ID = 1;
 
-    // 録音・音声ハードウェアの管理オブジェクト
     final AudioResources aRes = new AudioResources();
+    NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
         // 音量管理オブジェクトの初期化
         aRes.setAudioManager((AudioManager)getSystemService(Context.AUDIO_SERVICE));
+
+        // 通知の初期化
+        initNotification();
 
         // AudioRecordのハンドラ
         initHandler();
@@ -95,12 +107,45 @@ public class MainActivity extends AppCompatActivity {
                 if (aRes.getMicAccessAllowed()) {
                     textView_mode.setText(R.string.automationOn);
                     aRes.setAutoEnabled(true);
+                    postNotification();
                 }
             } else {
                 textView_mode.setText(R.string.automationOff);
                 aRes.setAutoEnabled(false);
+                cancelNotification();
             }
         });
+    }
+
+    private void initNotification() {
+        notificationManager =
+                (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel notificationChannel = new NotificationChannel(
+                NOTIFICATION_CHANNEL_ID, getString(R.string.app_name),
+                NotificationManager.IMPORTANCE_DEFAULT);
+        notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+
+    private void postNotification() {
+        Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("SmartVolumer")
+                .setContentText("音量の自動調整が有効")
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .setContentIntent(PendingIntent.getActivity(
+                        getApplicationContext(),
+                        NOTIFICATION_REQUEST_CODE,
+                        new Intent(getBaseContext(), MainActivity.class),
+                        PendingIntent.FLAG_IMMUTABLE
+                        )
+                ).build();
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void cancelNotification() {
+        notificationManager.cancel(NOTIFICATION_ID);
     }
 
     private void openSettings() {
